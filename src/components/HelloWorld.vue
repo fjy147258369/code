fff359e30f063025004c91b79ccc1fb584950911
fjy147258369 <2665315362@qq.com>
2020-06-05 18:04:29 +0800

    添加项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="hello">
+        <ul>
+            <li>
+                <label>账号：</label>
+                <div>
+                    <input type="text" />
+                </div>
+            </li>
+            <li>
+                <label>密码：</label>
+                <div>
+                    <input type="text" />
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HelloWorld"
+};
+</script>
+
+
+<style lang="less" scoped>
+.hello {
+    ul {
+        background: red;
+    }
+}
+</style>
